0f6b0b6ff6cc25bff8e1883c840dbfc4f946d9d5
KEN <shdnrnjs@gmail.com>
2023-09-29 12:25:36 +0900

    lv3 완 예정

--- tab.js
+++ tab.js
@@ -1,10 +1,30 @@
-let button = $('.tab-button') // 변수에 담아서 쓰는게 성능 상 좋다
+// for (let i = 0; i < $('.tab-button').length; i++){
 
-for(let i = 0; i<2; i++) {
-    $(button).eq(i).click(function(){
-        $(button).removeClass('orange');
-        $(button).eq(i).addClass('orange');
-        $(button).removeClass('show');
-        $(button).eq(i).addClass('show');
+//     $('.tab-button').eq(i).on('click', function(){
+//       탭열기(i)
+//     })
+  
+//   };
+  
+
+    $('.list').click(function(e){
+        // if(e.target == querySelectorAll('.tab=button')[0]) {
+        //     탭열기(0);
+        // }
+        // if(e.target == querySelectorAll('.tab=button')[1]) {
+        //     탭열기(1);
+        // }
+        // if(e.target == querySelectorAll('.tab=button')[2]) {
+        //     탭열기(2);
+        // }
+
+        탭열기(e.target.dataset.id); // 한 줄로 가능
+
     })
-}
+  
+  function 탭열기(구멍){
+    $('.tab-button').removeClass('orange');
+    $('.tab-button').eq(구멍).addClass('orange');
+    $('.tab-content').removeClass('show');
+    $('.tab-content').eq(구멍).addClass('show');
+  }
